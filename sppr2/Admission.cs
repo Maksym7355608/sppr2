2a1122e907be443be3c75c29b3d59ebc542803e2
Maksym <mmajor143@gmail.com>
2023-05-23 00:08:16 +0300

    Добавьте файлы проекта.

---
+++ sppr2/Admission.cs
@@ -0,0 +1,105 @@
+﻿using ClosedXML.Report;
+using Newtonsoft.Json;
+
+namespace sppr2
+{
+    internal class Admission
+    {
+        public static List<Abiturient> CreateAbbiturients(int count)
+        {
+            List<Abiturient> abbiturients = new(count);
+            Random rnd = new();
+            for (int i = 0; i < count; i++)
+            {
+                var benefit = rnd.Next(0, 4);
+                abbiturients.Add(new Abiturient(i, "Вступник №" + (i + 1).ToString(), rnd.Next(100, 201), rnd.Next(100, 201), rnd.Next(100, 201), benefit > 2));
+            }
+
+            return abbiturients;
+        }
+
+        public static List<Abiturient> PassSelection(List<Abiturient> abiturients)
+        {
+            List<Abiturient> passedAbiturients = new();
+
+            abiturients.Sort((a1, a2) => a2.Rating.CompareTo(a1.Rating));
+
+            int privilegeCount = default;
+
+            abiturients.ForEach(applicant =>
+            {
+                if (passedAbiturients.Count >= 350)
+                    return;
+
+                int maxBenefitiant = (int)(passedAbiturients.Count * 0.1);
+                if (applicant.Benefit && maxBenefitiant >= passedAbiturients.Count(x => x.Benefit))
+                {
+                    if (applicant.Rating >= 144 && applicant.Math >= 120)
+                    {
+                        passedAbiturients.Add(applicant);
+                        privilegeCount++;
+                    }
+                }
+                else if (applicant.Rating >= 160 && applicant.Math >= 140)
+                {
+                    passedAbiturients.Add(applicant);
+                }
+            });
+
+            return passedAbiturients;
+        }
+
+        public static void SaveToDb(string filePath, List<Abiturient> applicants)
+        {
+            string json = JsonConvert.SerializeObject(applicants, Formatting.Indented);
+
+            File.WriteAllText(filePath, json);
+        }
+
+        public static List<Abiturient> GetApplicantsFromDb(string filePath, List<Abiturient> applicants)
+        {
+            if (File.Exists(filePath))
+            {
+                string json = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<List<Abiturient>>(json ?? string.Empty) ?? new List<Abiturient>();
+
+            } else
+                return new List<Abiturient>();
+        }
+
+        public static void WriteModelToExcel<TModel>(TModel model, string templatePath, string filePath)
+        {
+            XLTemplate? template = new(templatePath);            
+            template.AddVariable(model);            
+            template.Generate();            
+            template.SaveAs(filePath);
+        }
+    }
+
+    public class Abiturient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Math { get; set; }
+        public double English { get; set; }
+        public double Ukrainian { get; set; }
+        public bool Benefit { get; set; }
+        public double Rating { get; set; }
+
+        public Abiturient(int id, string name, double math, double english, double ukrainian, bool benefit)
+        {
+            Id = id;
+            Name = name;
+            Math = math;
+            English = english;
+            Ukrainian = ukrainian;
+            Benefit = benefit;
+            Rating = 0.4 * math + 0.3 * english + 0.3 * ukrainian;
+        }
+    }
+
+    public class AbiturientDocumentModel
+    {
+        public List<Abiturient> Rows { get; set; }
+    }
+}
---
+++ sppr2/Program.cs
@@ -0,0 +1,98 @@
+﻿using sppr2;
+
+bool exit = false;
+var abiturients = new List<Abiturient>();
+var admittedApplicants = new List<Abiturient>();
+while (!exit)
+{
+    Console.Clear();
+    Console.WriteLine("Меню:");
+    Console.WriteLine("1. Додати абітурієнта");
+    Console.WriteLine("2. Видалити абітурієнта");
+    Console.WriteLine("3. Створити список абітурієнтів");
+    Console.WriteLine("4. Відібрати абітурієнтів");
+    Console.WriteLine("5. Зберегти список зарахованих абітурієнтів у файл Excel");
+    Console.WriteLine("6. Зберегти список зарахованих абітурієнтів у Базу даних");
+    Console.WriteLine("7. Зчитати список абітурієнтів з бази даних");
+    Console.WriteLine("8. Вийти");
+    Console.WriteLine();
+
+    Console.Write("Виберіть пункт меню: ");
+    string choice = Console.ReadLine();
+
+    switch (choice)
+    {
+        case "1":
+            AddApplicant();
+            break;
+        case "2":
+            RemoveApplicant();
+            break;
+        case "3":
+            abiturients.AddRange(Admission.CreateAbbiturients(5000));
+            break;
+        case "4":
+            admittedApplicants = Admission.PassSelection(abiturients);
+            break;
+        case "5":
+            var model = new AbiturientDocumentModel
+            {
+                Rows = admittedApplicants
+            };
+            Admission.WriteModelToExcel(model, @"Шаблон файлика", @"назва вихідного файлу");
+
+            break;
+        case "6":
+            Admission.SaveToDb("шлях до файла \"бд\"", abiturients);
+            break;
+        case "7":
+            Admission.GetApplicantsFromDb("шлях до файла \"бд\"", abiturients);
+            break;
+        case "8":
+            exit = true;
+            break;
+        default:
+            Console.WriteLine("Невірний вибір. Спробуйте ще раз.");
+            break;
+    }
+
+    Console.WriteLine();
+}
+
+void AddApplicant()
+{
+    Console.WriteLine("Додавання абітурієнта");
+    Console.WriteLine();
+
+    // Отримуємо дані абітурієнта від користувача
+    Console.Write("Введіть ім'я абітурієнта: ");
+    var name = Console.ReadLine();
+    Console.Write("Математика: ");
+    var math = Console.ReadLine();
+    Console.Write("Англ: ");
+    var eng = Console.ReadLine();
+    Console.Write("Укр: ");
+    var ukr = Console.ReadLine();
+    Console.Write("Пільга: ");
+    var previlegy = Console.ReadLine();
+
+    // Створюємо нового абітурієнта
+    var abiturient = new Abiturient(abiturients.Count > 0 ? abiturients.Max(x => x.Id) + 1 : 0, name, int.Parse(math), int.Parse(eng), int.Parse(ukr),
+        int.Parse(previlegy) != 0);
+
+    // Додаємо абітурієнта до списку
+    abiturients.Add(abiturient);
+
+    Console.WriteLine("Абітурієнта успішно додано до списку.");
+}
+
+void RemoveApplicant()
+{
+    Console.WriteLine("Введіть Id Абітурієнта: ");
+    var input = int.Parse(Console.ReadLine());
+    if (abiturients.Select(x => x.Id).Contains(input))
+        Console.WriteLine("Абітурієнта успішно видалено зі списку.");
+    else
+        Console.WriteLine("Абітурієнта не видалено зі списку.");
+
+}
